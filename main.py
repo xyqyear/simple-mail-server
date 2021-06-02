4aa962719bbb80e75ceb741cc0f423422585ef42
year xyq <xyqyear@gmail.com>
2021-06-02 14:36:16 +0800

    add main.py, first runnable version

---
+++ main.py
@@ -0,0 +1,28 @@
+import threading
+import logging
+
+import config
+
+from pop3 import POP3Server
+from smtp import SMTPServer
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+def main():
+    smtp_server = SMTPServer(config.domain, config.username, config.password)
+    pop3_server = POP3Server(config.username, config.password)
+
+    smtp_server_main_thread = threading.Thread(target=smtp_server.run)
+    pop3_server_main_thread = threading.Thread(target=pop3_server.run)
+
+    smtp_server_main_thread.start()
+    pop3_server_main_thread.start()
+
+    smtp_server_main_thread.join()
+    pop3_server_main_thread.join()
+
+
+if __name__ == '__main__':
+    main()
